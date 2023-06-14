2d562be5fd21b469d90f96a3931452930f3cffb0
ranjithmano13 <ranjithmano13@gmail.com>
2023-06-14 06:46:58 +0530

    added brand section

--- src/containers/header/Header.js
+++ src/containers/header/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./header.css";
-import people from '../../assets/people.png'
-import ai from '../../assets/ai.png'
+import people from "../../assets/people.png";
+import ai from "../../assets/ai.png";
 
 const Header = () => {
   return (
@@ -21,14 +21,13 @@ const Header = () => {
           <button type="button">Get Started</button>
         </div>
         <div className="gpt3__header-content__people">
-            <img src={people} alt="People"/>
-            <p>1,600 people requested access a visit in last 24 hours</p>
+          <img src={people} alt="People" />
+          <p>1,600 people requested access a visit in last 24 hours</p>
         </div>
-
       </div>
       <div className="gpt3__header-image">
-          <img src={ai} alt="ai"/>
-        </div>
+        <img src={ai} alt="ai" />
+      </div>
     </div>
   );
 };
